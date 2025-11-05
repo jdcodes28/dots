d685849e6100eb50997f922b06f657402d718fb5
jd <jediaz95@protonmail.com>
2025-11-05 02:11:40 -0600

    changed wallpaper randomizer

--- scripts/wallpaper_randomizer.sh
+++ scripts/wallpaper_randomizer.sh
@@ -2,18 +2,47 @@
 
 WALLPAPER_DIR=~/Pictures/Wallpapers
 INTERVAL=900
+USED_LIST="~/.cache/used_wallpapers.txt"
 WALLPAPER_DIR="${WALLPAPER_DIR/#\~/$HOME}"
 
 kill_swaybg() {
     pkill -9 swaybg 2>/dev/null
 }
 
+get_all_wallpapers() {
+    local dir="$1"
+    find "$dir" -maxdepth 1 -type f
+}
+
+get_unused_wallpaper() {
+    local dir="$1"
+
+    touch "$USED_LIST"
+
+    local all_wallpapers=$(get_all_wallpapers "$dir")
+    local total_count=$(echo "$all_wallpapers" | wc -l)
+    local unused=$(comm -23 <(echo "$all_wallpapers" | sort) <(sort "$USED_LIST"))
+    local unused_count=$(echo "$unused" | grep -c '^')
+
+    if [ -z "$unused" ] || [ "$unused_count" -eq 0 ]; then
+        unused="$all_wallpapers"
+        unused_count="$total_count"
+    fi
+}
+
 set_random_wallpaper() {
     local dir="$1"
-    
+
     kill_swaybg
-    find "$dir" -maxdepth 1 -type f -print0 | shuf -z -n 1 | xargs -0 -I {} sh -c 'swaybg -i "{}" -m stretch -o "*" &'
-    
+
+    local wallpaper=$(get_unused_wallpaper "$dir")
+
+    if [ -z "$wallpaper" ]; then
+        return 1
+    fi
+
+    echo "$wallpaper" >> "$USED_LIST"
+    swaybg -i "$wallpaper" -m stretch -o "*" &
     sleep 0.25
 }
 
